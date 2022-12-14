6b11e4384ea1a81e8b909ebea4e4a492e95b678f
Rafael Dantas <dantas.rafael8928@gmail.com>
2022-12-14 18:24:21 -0300

    Configurando a Home do site

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,16 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+
 
 function Home() {
-    return <h1>Home</h1>
+    return(
+        <section>
+            <h1>Bem-Vindo ao <span>Costs</span></h1>
+            <p>Comece a gerenciar os seu projetos agora mesmo!</p>
+            <a href='/'>Criar Projeto</a>
+            <img src={savings} alt='Costs'/>
+        </section>
+    )
 }
 
 export default Home
