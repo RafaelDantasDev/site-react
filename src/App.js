a134f09743547700e45b8db55b476f72f3ba9899
Rafael Dantas <dantas.rafael8928@gmail.com>
2022-12-14 12:30:19 -0300

    Adicionando mais um link na barra de navegação

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar'
@@ -16,6 +17,7 @@ function App() {
       <Switch>
         <Container customClass='min-height'>
           <Route exact path='/'><Home/></Route>
+          <Route exact path='/projects'><Projects/></Route>
           <Route exact path='/company'><Company/></Route>
           <Route exact path='/contact'><Contact/></Route>
           <Route exact path='/newproject'><NewProject/></Route>
