3d333abcdf5a67e1e18bc429afd560afaf00a8c7
Rafael Dantas <dantas.rafael8928@gmail.com>
2022-12-16 10:53:56 -0300

    Estilizando os inputs do site

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -3,8 +3,8 @@ import styles from './Input.module.css'
 function Input({type, text, name, placeholder, handleOnchange, value}) {
     return(
         <div className={styles.form_control}>
-            <label htmlFor=''></label>
-            <input type='text' />
+            <label htmlFor={name}>{text}:</label>
+            <input type={type} name={name} id={name} placeholder={placeholder} onChange={handleOnchange} value={value} />
         </div>
     )
 }
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,10 +1,11 @@
 import styles from './ProjectForm.module.css'
+import Input from '../form/Input'
 
 function ProjectForm() {
     return (
        <form className={styles.form}>
-            <div><input type='text' placeholder='Insira o nome do projeto'/></div>
-            <div><input type='number' placeholder='Insira o orçamento total'/></div>
+            <Input type='text' text='Nome do Projeto' name='name' placeholder='Insira o nome do projeto' />
+            <Input type='number' text='Orçamento do Projeto' name='budget' placeholder='Insira o orçamento total' />
             <div>
                 <select name='category_id'>
                     <option disable selected>Selecione a categoria</option>
