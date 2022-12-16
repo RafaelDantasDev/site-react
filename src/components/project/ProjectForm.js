babaa904f6dd09b290f701ce00c13d43090d335f
Rafael Dantas <dantas.rafael8928@gmail.com>
2022-12-16 11:09:57 -0300

    Estilizando o select option do site

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,16 +1,13 @@
 import styles from './ProjectForm.module.css'
 import Input from '../form/Input'
+import Select from '../form/Select'
 
 function ProjectForm() {
     return (
        <form className={styles.form}>
             <Input type='text' text='Nome do Projeto' name='name' placeholder='Insira o nome do projeto' />
             <Input type='number' text='Orçamento do Projeto' name='budget' placeholder='Insira o orçamento total' />
-            <div>
-                <select name='category_id'>
-                    <option disable selected>Selecione a categoria</option>
-                </select>
-            </div>
+            <Select name='category_id' text='Selecione a categoria'/>
             <div><input type='submit' value='Criar Projeto'/></div>
        </form>
     )
