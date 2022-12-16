9f0abdda26dca30da97cad490b94ed42e1085cd6
Rafael Dantas <dantas.rafael8928@gmail.com>
2022-12-16 10:38:20 -0300

    Estilização de formulário

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,8 +1,8 @@
-
+import styles from './ProjectForm.module.css'
 
 function ProjectForm() {
     return (
-       <form>
+       <form className={styles.form}>
             <div><input type='text' placeholder='Insira o nome do projeto'/></div>
             <div><input type='number' placeholder='Insira o orçamento total'/></div>
             <div>
