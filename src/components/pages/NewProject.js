8df8901b1669368019169cdda07cecc2f32d49e8
Rafael Dantas <dantas.rafael8928@gmail.com>
2022-12-16 10:12:04 -0300

    Estilização de botões, páginas e projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,7 +1,14 @@
-
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
 
 function NewProject() {
-    return <h1>New Project</h1>
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Cria seu projeto para depois adicionar os serviços</p>
+            <ProjectForm/>
+        </div>
+    )
 }
 
 export default NewProject
