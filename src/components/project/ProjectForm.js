9b99cc724bd3c5db887512233d8b9175b41dc414
Rafael Dantas <dantas.rafael8928@gmail.com>
2022-12-19 13:04:52 -0300

    Configurando o banco de dados do site

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,14 +1,15 @@
 import styles from './ProjectForm.module.css'
 import Input from '../form/Input'
 import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
 
-function ProjectForm() {
+function ProjectForm({btnText}) {
     return (
        <form className={styles.form}>
             <Input type='text' text='Nome do Projeto' name='name' placeholder='Insira o nome do projeto' />
             <Input type='number' text='Orçamento do Projeto' name='budget' placeholder='Insira o orçamento total' />
             <Select name='category_id' text='Selecione a categoria'/>
-            <div><input type='submit' value='Criar Projeto'/></div>
+            <SubmitButton text={btnText}/>
        </form>
     )
 }
