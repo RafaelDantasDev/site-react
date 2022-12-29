8cd819f037cbfeec6ae60075485b9da8f0802e17
Rafael Dantas <dantas.rafael8928@gmail.com>
2022-12-29 12:36:25 -0300

    Criando Dashbord

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,9 @@
 import { useLocation } from "react-router-dom"
 
 import Message from "../layout/Message"
+import Container from '../layout/Container'
 
+import styles from './Projects.module.css'
 
 function Projects() {
 
@@ -13,8 +15,14 @@ function Projects() {
 
     return(
         <div>
+           <div>
             <h1>Meus Projetos</h1>
+            <a href="#">Novo projeto</a>
+           </div>
             {message && <Message type='sucess' msg='Alguma coisa'/>}
+            <Container customClass='start'>
+                <p>Projetos</p>
+            </Container>
         </div>
     )
 }
